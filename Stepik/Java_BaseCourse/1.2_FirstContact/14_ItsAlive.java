f20be1b9e164cbdcb6ce5607d46778eae82ac22e
Ivan Obydennov <ivan.obydennov@gmail.com>
2022-01-15 13:24:03 +0700

    Добавлены решения уроков 1.1-2.3

---
+++ Stepik/Java_BaseCourse/1.2_FirstContact/14_ItsAlive.java
@@ -0,0 +1,10 @@
+/**
+* Напишите программу, выводящую в консоль It's alive! It's alive!
+* Класс должен называться "Main", это ограничение проверяющей системы.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("It's alive! It's alive!");
+    }
+}
