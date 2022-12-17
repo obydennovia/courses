9173ec919ae51e64d6284f17302b30e706a62404
Ivan Obydennov <ivan.obydennov@gmail.com>
2022-12-17 16:23:33 +0700

    Первый модуль Skillbox - Java-разработчик с нуля

---
+++ Skillbox/Java developer from scratch/01 Introductory module/02 FillingStation/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Система расчёта стоимости топлива");
+
+        int fuelType = 95;  // Тип бензина
+        int amount = 50;    // Количество литров бензина
+
+        double fuel92price = 60.2;  // Цена 92 бензина
+        double fuel95price = 67.33; // Цена 95 бензина
+
+        double fuelPrice = 0;
+
+        if (fuelType == 92) {
+            fuelPrice = fuel95price;
+        }
+
+        else if (fuelType == 95) {
+            fuelPrice = fuel95price;
+        } else {
+            System.out.println("Указан неверный тип топлива");
+        }
+
+        if (amount < 1) {
+            System.out.println("Указано слишком малое количество топлива");
+            amount = 0;
+        }
+
+        System.out.println("Цена выбранного топлива: " + fuelPrice + " руб.");
+
+        double totalPrice = fuelPrice * amount;
+        System.out.println("Общая стоимость заправки: " + totalPrice + " руб.");
+    }
+}
