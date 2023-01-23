be70c39ed82a9c858cdc0c0789c3228bbddd0dae
Ivan Obydennov <ivan.obydennov@gmail.com>
2023-01-24 03:02:58 +0700

    Исправленное задание

--- Skillbox/Java developer from scratch/07 Primitives/Task 7_9/Primitives/IntOrInteger/src/Main.java
+++ Skillbox/Java developer from scratch/07 Primitives/Task 7_9/Primitives/IntOrInteger/src/Main.java
@@ -1,15 +1,24 @@
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
 
+        char c;
+
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
-        for (char c = 'А'; c <= 'я'; c++) {
-            System.out.println(c + " = " + (int) c);
+        for (long i = 0L; i <= 4_294_967_295L; i++)
+        {
+            if (((char) i >= 'А') && ((char) i <= 'я')) {
+                System.out.println((char) i + " = " + i);
+            }
+            if (((char) i == 'Ё') || ((char) i == 'ё')) {
+                System.out.println((char) i + " = " + i);
+            }
         }
-
     }
 }
