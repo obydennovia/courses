0dfc5d4fad3a7985d2d200eedf67e5f5793a9ba1
Ivan Obydennov <93914703+obydennovia@users.noreply.github.com>
2024-02-07 00:51:40 +0700

    Добавлено решение задачи task_2.py

--- Skillbox/Python Basics. Part 1/Module 10. Nested Loops/task_2.py
+++ Skillbox/Python Basics. Part 1/Module 10. Nested Loops/task_2.py
@@ -9,3 +9,10 @@ print('Задача 2. Лестница')
 # 3 3 3
 # 4 4 4 4
 # 5 5 5 5 5
+
+N = int(input("Введите число: "))
+for row in range(1, N + 1):
+  for col in range(1, N + 1):
+    if row >= col:
+      print(row, end = " ")
+  print()
