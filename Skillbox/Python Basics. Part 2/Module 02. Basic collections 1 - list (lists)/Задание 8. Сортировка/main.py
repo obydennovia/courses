c3ecec6ec6dba34d64ad67a9b2de5ccf083faffd
Ivan Obydennov <ivan.obydennov@gmail.com>
2024-02-07 16:16:00 +0700

    Добавлено решение задачи модуля 2 - 8 и удалена лишняя папка из модуля 1

---
+++ Skillbox/Python Basics. Part 2/Module 02. Basic collections 1 - list (lists)/Задание 8. Сортировка/main.py
@@ -0,0 +1,10 @@
+def selection_sort(nums):
+    for i_mn in range(len(nums)):
+        for curr in range(i_mn, len(nums)):
+            if nums[curr] < nums[i_mn]:
+                nums[curr], nums[i_mn] = nums[i_mn], nums[curr]
+    return nums
+
+nums = [4, 9, 7, 6, 3, 2]
+
+print(selection_sort(nums))
